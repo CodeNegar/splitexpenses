378665e1f9bc5589ce9b27de2c37b1030e482fab
Farhad Ahmadi <ahm.farhad@gmail.com>
2018-06-04 23:44:18 +0800

    App hello world added

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express');
-const app = express();
-
+const exphbs  = require('express-handlebars');
 const port = 3000;
 
+const app = express();
+
+// Register Handlebars middleware
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-	res.send('Hello World');
+	res.render('index');
 });
 
-app.listen(port, ()=>{
-	console.log('Split Expenses app started');
-})
+app.listen(port, () => {
+    console.log('Split Expenses app started');
+});
